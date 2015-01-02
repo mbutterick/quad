ea4e8972c44ee7bbf3acb838e6c5086c3926179a
Matthew Butterick <mb@mbtype.com>
2015-01-02 12:52:16 -0800

    add dependency on hyphenate

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "quad")
-(define deps '(("base" #:version "6.0") "sugar" "csp" "rackunit-lib"))
+(define deps '(("base" #:version "6.0") "sugar" "csp" "rackunit-lib" "hyphenate"))
 (define update-implies '("sugar"))
 (define compile-omit-paths '("tests.rkt" "tests-ocm.rkt"))
