0b1e3d0854e20a2fd5b3fd5e0e8f66a4f3680e0b
Matthew Butterick <mb@mbtype.com>
2015-03-23 13:48:43 -0700

    profiling

--- main-typed-sample.rkt
+++ main-typed-sample.rkt
@@ -1,8 +1,8 @@
 #lang typed/racket/base
-(require "main-typed.rkt" "logger-typed.rkt" "world-typed.rkt" "samples-typed.rkt")
+(require "main-typed.rkt" "logger-typed.rkt" "world-typed.rkt" "samples-typed.rkt" "quads-typed.rkt")
 
-#;(require/typed contract-profile
-                [contract-profile-thunk ((-> Any) . -> . Quad)])
+(require/typed profile
+                [profile-thunk ((-> Any) [#:delay Float]  . -> . Quad)])
 
 (require "render-typed.rkt" typed/racket/class)
 (activate-logger quad-logger)
@@ -11,5 +11,5 @@
                [world:paper-width-default 600.0]
                [world:paper-height-default 700.0])
   (define sample (ti5))
-  (define to (time (typeset sample)))
+  (define to (time (profile-thunk #:delay 0.001 (λ () (typeset sample)))))
   (time (send (new pdf-renderer%) render-to-file to "foo-typed.pdf")))
