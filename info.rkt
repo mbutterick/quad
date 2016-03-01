4f01e95523916eac764cdc1af31b4f0ac4e43f19
Matthew Butterick <mb@mbtype.com>
2016-03-01 06:56:04 -0800

    deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection 'multi)
 (define deps '(("base" #:version "6.0") "sugar" "csp" "rackunit-lib" "hyphenate" "at-exp-lib" "data-lib" "draw-lib" "gui-lib" "math-lib" "plot-gui-lib" "plot-lib" "profile-lib" "typed-racket-lib"))
-(define build-deps '("racket-doc" "scribble-lib"))
+(define build-deps '("racket-doc" "scribble-lib" "draw-doc" "scribble-doc"))
 (define update-implies '("sugar"))
