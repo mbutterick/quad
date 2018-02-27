595f74b10f3d0fcabd15bd7a0d167fb3cfd8062f
Matthew Butterick <mb@mbtype.com>
2018-02-27 12:44:21 -0800

    depend

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 (define collection 'multi)
 (define deps '("beautiful-racket-lib"
                "txexpr"
+               "pitfall"
                ("base" #:version "6.0") "sugar" "csp" "rackunit-lib" "hyphenate" "at-exp-lib" "data-lib" "draw-lib" "gui-lib" "math-lib" "plot-gui-lib" "plot-lib" "profile-lib" "typed-racket-lib"))
 (define build-deps '("debug"
                      "racket-doc" "scribble-lib" "draw-doc" "scribble-doc"))
