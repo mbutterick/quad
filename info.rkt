706161acf4b7610e8db6f142c5199635f9843e19
Matthew Butterick <mb@mbtype.com>
2018-11-15 18:30:33 -0800

    deps

--- info.rkt
+++ info.rkt
@@ -1,10 +1,12 @@
 #lang info
-(define collection 'multi)
-(define deps '("pict-lib"
+(define collection 'multi)(define deps '("at-exp-lib"
+               "base"
                "beautiful-racket-lib"
-               "txexpr"
+               "fontland"
+               "hyphenate"
                "pitfall"
-               ("base" #:version "6.0") "sugar" "csp" "rackunit-lib" "hyphenate" "at-exp-lib" "data-lib" "draw-lib" "gui-lib" "math-lib" "plot-gui-lib" "plot-lib" "profile-lib" "typed-racket-lib"))
-(define build-deps '("debug"
-                     "racket-doc" "scribble-lib" "draw-doc" "scribble-doc"))
-(define update-implies '("sugar"))
+               "pollen"
+               "rackunit-lib"
+               "sugar"
+               "txexpr"))
+(define build-deps '("debug"))
