4980d7b4b856061f8e04c550710ecdc5f08f8659
Matthew Butterick <mb@mbtype.com>
2019-01-22 15:25:11 -0800

    and that

--- qtest/fark.rkt
+++ qtest/fark.rkt
@@ -1,16 +1,9 @@
 #lang qtest/markdown
 
-## 1. Welcome to Racket
+> what could happen on two lines that doesn't happen in one
 
-Depending on how you look at it, **Racket** is
 
-* a _programming language_—a dialect of Lisp and a descendant of Scheme;
-
-  > See Dialects of Racket and Scheme for more information on other
-  > dialects of Lisp and how they relate to Racket.
-
-* a _family_ of programming languages—variants of Racket, and more; or
-
-* a set of _tools_—for using a family of programming languages.
-
-Where there is no room for confusion, we use simply _Racket_.
+```
+tell me
+I dono
+```
