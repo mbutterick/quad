ad5630a8bc6bf3f81ac07ed03698d19c3e7cd524
Matthew Butterick <mb@mbtype.com>
2019-05-01 13:39:06 -0700

    don't add paragraph breaks when single quad exists

--- quadwriter/markdown.rkt
+++ quadwriter/markdown.rkt
@@ -21,14 +21,16 @@
                  [(? null?) '(" ")] ; single nonbreaking space, so something prints
                  [strs strs]))
   ;; markdown parser returns list of paragraphs
-  (root null (add-between strs (list qexpr-para-break)
-                          #:before-first (list qexpr-para-break)
-                          #:after-last (list qexpr-para-break)
-                          #:splice? #true)))
+  (root null (match strs
+               [(list str) strs]
+               [_ (add-between strs (list qexpr-para-break)
+                               #:before-first (list qexpr-para-break)
+                               #:after-last (list qexpr-para-break)
+                               #:splice? #true)])))
 (make-module-begin doc-proc)
 
 (module reader racket/base
   (require racket/port markdown "lang-helper.rkt")
   (provide read-syntax get-info)
   (define read-syntax (make-read-syntax 'quadwriter/markdown
-                       (λ (path-string p) (xexpr->parse-tree (parse-markdown (port->string p)))))))
+                                        (λ (path-string p) (xexpr->parse-tree (parse-markdown (port->string p)))))))
