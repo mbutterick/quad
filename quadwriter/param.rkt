18859294db96105cc8b979ec19df84f94cf95729
Matthew Butterick <mb@mbtype.com>
2019-05-02 16:12:31 -0700

    some margins

--- quadwriter/param.rkt
+++ quadwriter/param.rkt
@@ -4,7 +4,7 @@
 (define current-pdf (make-parameter #f))
 (define current-locale (make-parameter 'us))
 
-(define draw-debug? (make-parameter #f))
+(define draw-debug? (make-parameter #t))
 (define draw-debug-line? (make-parameter #t))
 (define draw-debug-block? (make-parameter #t))
 (define draw-debug-string? (make-parameter #t))
