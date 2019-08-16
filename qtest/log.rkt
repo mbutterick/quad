215bdcfaa603086fc553b03255a115405669b5e0
Matthew Butterick <mb@mbtype.com>
2019-08-16 10:58:58 -0700

    raco qtest

---
+++ qtest/log.rkt
@@ -0,0 +1,4 @@
+#lang racket/base
+(require racket/logging)
+(provide (all-defined-out))
+(define-logger qtest)
---
+++ qtest/raco.rkt
@@ -0,0 +1,15 @@
+#lang debug racket/base
+(require racket/logging
+         racket/match
+         "log.rkt")
+
+(with-logging-to-port
+    (current-error-port)
+  (λ ()
+    (match (with-handlers ([exn:fail? (λ (exn) #f)])
+             (vector-ref (current-command-line-arguments) 0))
+      ["test" (dynamic-require 'qtest/all #f)]
+      [_ (displayln "no cmd given")]))
+  #:logger qtest-logger
+  'info
+  'qtest)
