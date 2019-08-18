f11d386c20c1bbd292a6bdd85f878447c23cc40f
Matthew Butterick <mb@mbtype.com>
2019-08-18 12:02:30 -0700

    start vectorizing

--- quad/proto.rkt
+++ quad/proto.rkt
@@ -3,11 +3,11 @@
          (for-syntax racket/syntax))
 (provide (all-defined-out))
 
-(define proto (make-hasheq))
+(define proto (make-vector 1 '(proto)))
 (define (proto-ref d k) (dict-ref d k #false))
 (define (proto-set! d k v) (dict-set! d k v) d)
 (define (proto? x) (dict? x))
-(define type-key '$type)
+(define type-key 0)
 
 (define-syntax (define-prototype stx)
   (syntax-case stx ()
@@ -24,33 +24,34 @@
            (when BASE-TYPE
              (unless (proto? BASE-TYPE)
                (raise-argument-error 'define-prototype "base type must derive from proto" BASE-TYPE)))
+           
            (...
             (define-syntax-rule (TYPE-COPY PID [K2 V2] ...)
-             (let ([d (dict-copy PID)])
-               (for ([k (in-list (list 'K2 ...))]
-                     [v (in-list (list V2 ...))])
-                 (proto-set! d k v))
-               d)))
-           (define MAKE-TYPE
-             (make-keyword-procedure 
-              (λ (kws kwargs . rest)
-                (define allowed-ks (list 'K ...))
-                (define d (TYPE-COPY BASE-TYPE [K V] ...))
-                (for ([kw (in-list kws)]
-                      [kwarg (in-list kwargs)])
-                  (define k (string->symbol (keyword->string kw)))
-                  #;(unless (memq k allowed-ks)
-                    (raise-argument-error 'MAKE-TYPE (format "valid keyword arg ~v" (map string->keyword (map symbol->string allowed-ks))) kw))
-                  (proto-set! d k kwarg))
-                ;; set type last so it overrides anything else
-                (proto-set! d type-key (cons 'TYPE (dict-ref BASE-TYPE type-key null))))))
+              (let ([d (vector-append PID (make-vector (length (list 'K2 ...))))])
+                (for ([(k ki) (in-indexed (list 'K2 ...))]
+                      [v (in-list (list V2 ...))])
+                  (proto-set! d (+ ki (vector-length BASE-TYPE)) v))
+                d)))
+
+           (define allowed-ks (reverse (list 'K ...)))
+           (define (MAKE-TYPE #:printable [printable #f]
+                              #:id [id #f]
+                              #:draw [draw #f]
+                              #:draw-end [draw-end #f]
+                              #:from [from #f]
+                              #:elems [elems null])
+                              
+             (define d (TYPE-COPY BASE-TYPE [K V] ...))
+             
+             ;; set type last so it overrides anything else
+             (proto-set! d type-key (cons 'TYPE (dict-ref BASE-TYPE type-key null))))
            (define (K-GET d) (proto-ref d 'K)) ...
-           (define (K-SET d v) (proto-set! d 'K v)) ...
+           (define (K-SET d v) (proto-set! d (length (memq 'K allowed-ks)) v)) ...
            (define TYPE (MAKE-TYPE))
-           (define (TYPE? x) (and (dict? x) (memq 'TYPE (dict-ref x type-key null)) #true))))]))
+           (define (TYPE? x) (and (proto? x) (memq 'TYPE (dict-ref x type-key null)) #true))))]))
 
 (define-prototype x) 
-(define-prototype foo [xs null])
+(define-prototype foo [xs 'hoo])
 (define-prototype bar foo [b 42])
 (module+ test
   (require rackunit)
@@ -63,7 +64,7 @@
 
 (module+ test
   (make-bar)
-  (proto-set! foo 'z 12)
+  (set-foo-xs! foo 12)
   (make-bar))
 
 (define (proto-equal? d1 d2)
