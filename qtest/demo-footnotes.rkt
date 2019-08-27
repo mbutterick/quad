c7a419f16daeba3fc113ded8ec713339c1ff1320
Matthew Butterick <mb@mbtype.com>
2019-08-27 07:08:58 -0700

    typo

--- qtest/demo-footnotes.rkt
+++ qtest/demo-footnotes.rkt
@@ -12,6 +12,6 @@
 
 " world." '(q ((fn-ref "2")) "†")
 
-'(q ((flow "footnote")(fn-text "2")(font-size-adjust "80%")(line-heightr-adjust "70%")) (q ((fn-text-start "2")) "†") "An instance of link-element has a tag for the target of the link." (q ((break "para"))))
+'(q ((flow "footnote")(fn-text "2")(font-size-adjust "80%")(line-height-adjust "70%")) (q ((fn-text-start "2")) "†") "An instance of link-element has a tag for the target of the link." (q ((break "para"))))
 
 " I love you."
