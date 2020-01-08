15d11a640f15b833680b2958128aec2b9b5ba2c1
Matthew Butterick <mb@mbtype.com>
2020-01-07 20:26:52 -0800

    stub harder

--- quad/lang.rkt
+++ quad/lang.rkt
@@ -1,8 +1,24 @@
 #lang racket/base
-(require (for-syntax racket/base))
+(require (for-syntax racket/base)
+         quad
+         (prefix-in doclang: "doclang-raw.rkt"))
 (provide (rename-out [quad-mb #%module-begin])
-         #%top-interaction)
+         #%top-interaction
+         #%top
+         #%app
+         #%datum
+         (except-out (all-from-out racket/base) #%module-begin))
 
+(define-for-syntax export-name 'quad)
 (define-syntax (quad-mb stx)
   (syntax-case stx ()
-    [(_ . ARGS) #'(#%module-begin . ARGS)]))
+    [(_ . ARGS)
+     (with-syntax ([EXPORT (datum->syntax stx export-name)])
+       #'(doclang:#%module-begin
+          EXPORT ; positional arg for doclang-raw: name of export
+          (λ (xs)
+            (define x (q #:elems xs))
+            (println x)
+            x) ;  positional arg for doclang-raw: post-processor
+          (provide EXPORT)
+          (begin . ARGS)))]))
