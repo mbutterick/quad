59cd83deced03af15a44ed4e6c4c46183f333c68
Matthew Butterick <mb@mbtype.com>
2020-01-27 14:29:07 -0800

    test font setup

---
+++ qtest/test-font-setup.rkt
@@ -0,0 +1,5 @@
+#lang quadwriter
+
+'(q ((font-family "arbitrary-name")(font-size "24")) "This text is in Charter")
+'(q ((break "para")))
+'(q ((font-family "charter")(font-size "24")) "This text is in Charter too")
